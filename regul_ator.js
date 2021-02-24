c58f2b073b091441ca8219ef31d34847b8d1a21e
rizemun <rizeman01@gmail.com>
2021-02-25 00:47:36 +0300

    :construction: add download link

--- regul_ator.js
+++ regul_ator.js
@@ -2,8 +2,8 @@
 // @name          Oggetto Redmine regul'ator
 // @namespace     rizemun.oggetto
 // @version       0.1
-// @downloadURL
-// @updateURL
+// @downloadURL   https://raw.githubusercontent.com/rizemun/redmine-regulator/master/regul_ator.js
+// @updateURL     https://raw.githubusercontent.com/rizemun/redmine-regulator/master/regul_ator.js
 // @description   Add time-tracking assistant
 // @author        Artem Markin <rizeman01@gmail.com>
 // @match         https://redmine.oggettoweb.com/issues/*/time_entries/*
